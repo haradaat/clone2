7b0fb1039a6e1dd95150af1243ab772a82e190e1
原田篤 <haradaatsushi@haradaatsushinoMacBook-Air.local>
2021-05-21 16:31:07 +0900

    ブログ編集画面の作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,18 +6,29 @@ class PostsController < ApplicationController
       @post = Post.new
     end
     def create
-      @post = Post.new(post_params)
-      if @post.save
-        redirect_to new_post_path, notice: "ブログを作成しました！"
-      else
-        render :new
-      end
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to posts_path, notice: "ブログを作成しました！"
+        else
+            render :new
+        end
     end
     def show
         @post = Post.find(params[:id])
+    end
+    def edit
+        @post = Post.find(params[:id])
+    end
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to posts_path, notice: "ブログを編集しました！"
+        else
+            render :edit
+        end
     end
     private
     def post_params
         params.require(:post).permit(:content)
     end
-  end
+end
